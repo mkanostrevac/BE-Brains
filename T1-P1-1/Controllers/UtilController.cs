e3850deccb4b80b6179d969287fedeb41b4d9829
stawross <mkanostrevac@gmail.com>
2018-04-11 22:35:20 +0200

    T1 updated

---
+++ T1-P1-1/Controllers/UtilController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace T1_P1_1.Controllers
+{
+    [RoutePrefix("util")]
+    public class UtilController : ApiController
+    {
+        [Route("date")]
+        public string GetDate()
+        {
+            return DateTime.Now.ToString();
+        }
+
+        [Route("family")]
+        public IEnumerable<string> GetFamily()
+        {
+            return new string[] { "Marko", "Ivan", "Jelena" };
+        }
+
+        [Route("myclass")]
+        public HttpResponseMessage GetMyClass()
+        {
+            string[] polaznici = new string[] { "Marko Markovic", "Ivana Ivanovic", "Nikola Nikolic" };
+            string content = "<h1> Moja grupa </h1> ";
+
+            foreach (string polaznik in polaznici)
+            {
+                content += "<p>" + polaznik + "</p>";
+            }
+
+            HttpResponseMessage response = new HttpResponseMessage();
+            response.Content = new StringContent(content);
+            response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("text/html");
+
+            return response;
+        }
+    }
+}
