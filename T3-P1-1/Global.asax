a6c51f5e12754d02822926a59b60b92f191d6aa1
stawross <mkanostrevac@gmail.com>
2018-04-20 18:45:21 +0200

    Added T3-P1-1

---
+++ T3-P1-1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="T3_P1_1.WebApiApplication" Language="C#" %>
---
+++ T3-P1-1/Models/DataAccessContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace T3_P1_1.Models
+{
+    public class DataAccessContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DataAccessContext(): 
+            base("DataAccessConnection")
+        {
+            Database.SetInitializer<DataAccessContext>(new DropCreateDatabaseIfModelChanges<DataAccessContext>());
+        }
+    }
+}
